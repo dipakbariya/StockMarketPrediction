77ffd8bb2a96ddd3e1b988333e1d3abc492fb022
dipakbariya <63631974+dipakbariya@users.noreply.github.com>
2021-10-13 08:50:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,15 +265,15 @@ def predict():
     text_file.close()
     HTML(test.to_html(classes='table table-striped'))
     
-    from csv import writer
-    def append_list_as_row(file_name, list_of_elem):
-        # Open file in append mode
-        with open(file_name, 'a+', newline='') as write_obj:
-            # Create a writer object from csv module
-            csv_writer = writer(write_obj)
-            # Add contents of list as last row in the csv file
-            csv_writer.writerow(list_of_elem)
-    append_list_as_row("Twitter_stock_final_dataset.csv", df_all.iloc[0,:])
+#     from csv import writer
+#     def append_list_as_row(file_name, list_of_elem):
+#         # Open file in append mode
+#         with open(file_name, 'a+', newline='') as write_obj:
+#             # Create a writer object from csv module
+#             csv_writer = writer(write_obj)
+#             # Add contents of list as last row in the csv file
+#             csv_writer.writerow(list_of_elem)
+#     append_list_as_row("Twitter_stock_final_dataset.csv", df_all.iloc[0,:])
     
     k= pd.read_csv("Twitter_stock_final_dataset.csv")
     k["Date"] = pd.to_datetime(k[['Day','Month','Year']])
